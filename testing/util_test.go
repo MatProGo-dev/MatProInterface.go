22fc6f9dd013443a3fb816990351b919e044922c
Kwesi Rutledge <kwesir@mit.edu>
2023-05-23 23:18:57 -0400

    Moved Tests for Dot() into operators_test.go

--- testing/util_test.go
+++ testing/util_test.go
@@ -5,48 +5,6 @@ import (
 	"testing"
 )
 
-func TestDot(t *testing.T) {
-	N := 10
-	m := optim.NewModel("testDot")
-	xs := m.AddBinaryVariableVector(N)
-	coeffs := make([]float64, N)
-
-	for i := 0; i < N; i++ {
-		coeffs[i] = float64(i + 1)
-	}
-
-	expr := optim.Dot(xs.Elements, coeffs)
-
-	for i, coeff := range expr.Coeffs() {
-		if coeffs[i] != coeff {
-			t.Errorf("Coeff mismatch: %v != %v", coeff, coeffs[i])
-		}
-	}
-
-	if expr.Constant() != 0 {
-		t.Errorf("Constant mismatch: %v != 0", expr.Constant())
-	}
-}
-
-func TestDotPanic(t *testing.T) {
-	N := 10
-	m := optim.NewModel("TestDotPanic")
-	xs := m.AddBinaryVariableVector(N)
-	coeffs := make([]float64, N-1)
-
-	for i := 0; i < N-1; i++ {
-		coeffs[i] = float64(i + 1)
-	}
-
-	defer func() {
-		if r := recover(); r == nil {
-			t.Error("Coeff size mismatch: Code did not panic")
-		}
-	}()
-
-	optim.Dot(xs.Elements, coeffs)
-}
-
 func TestSumVars(t *testing.T) {
 	numVars := 3
 	m := optim.NewModel("TestSumVars")
