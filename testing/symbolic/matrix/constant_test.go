fcaeefd590d233ffd1b1367105ac5cc51fe1f317
Kwesi Rutledge <kwesir@mit.edu>
2023-11-01 12:31:42 -0400

    Adding Tests for NumVars and IDs for matrix.Constant

--- testing/symbolic/matrix/constant_test.go
+++ testing/symbolic/matrix/constant_test.go
@@ -1,5 +1,10 @@
 package matrix_test
 
+import (
+	"github.com/MatProGo-dev/MatProInterface.go/symbolic/matrix"
+	"testing"
+)
+
 /*
 constant_test.go
 Description:
@@ -7,5 +12,40 @@ Description:
 */
 
 /*
+TestConstant_NumVars1
+Description:
 
- */
+	Tests that NumVars() returns 0 for a matrix.Constant
+*/
+func TestConstant_NumVars1(t *testing.T) {
+	// Constants
+	mat1 := matrix.Constant(matrix.Zeros(2, 3))
+
+	// Algorithm
+	if mat1.NumVars() != 0 {
+		t.Errorf(
+			"mat1.NumVars() = %v =/= 0",
+			mat1.NumVars(),
+		)
+	}
+}
+
+/*
+TestConstant_IDs1
+Description:
+
+	Tests that IDs() returns an empty slice of []uint64{}
+*/
+func TestConstant_IDs1(t *testing.T) {
+	// Constants
+	mat1 := matrix.Constant(matrix.Zeros(2, 3))
+
+	// Algorithm
+	ids1 := mat1.IDs()
+	if len(ids1) != 0 {
+		t.Errorf(
+			"len(ids1) = %v =/= 0",
+			len(ids1),
+		)
+	}
+}
