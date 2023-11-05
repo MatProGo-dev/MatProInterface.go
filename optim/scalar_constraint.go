37b97f4197c081edfe144eea278b726526c8273f
Kwesi Rutledge <kwesir@mit.edu>
2023-11-05 09:12:55 -0500

    Added New Method for Detecting Whether or Not a Constraint Is Linear

--- optim/scalar_constraint.go
+++ optim/scalar_constraint.go
@@ -17,6 +17,29 @@ func (sc ScalarConstraint) Right() Expression {
 	return sc.RightHandSide
 }
 
+/*
+IsLinear
+Description:
+
+	Describes whether or not a given linear constraint is
+	linear or not.
+*/
+func (sc ScalarConstraint) IsLinear() (bool, error) {
+	// Check left and right side.
+	if _, tf := sc.LeftHandSide.(ScalarQuadraticExpression); tf {
+		return false, nil
+	}
+
+	// If left side has degree less than two, then this only depends
+	// on the right side.
+	if _, tf := sc.RightHandSide.(ScalarQuadraticExpression); tf {
+		return false, nil
+	}
+
+	// Otherwise return true
+	return true, nil
+}
+
 // ConstrSense represents if the constraint x <= y, x >= y, or x == y. For easy
 // integration with Gurobi, the senses have been encoding using a byte in
 // the same way Gurobi encodes the constraint senses.
--- testing/optim/scalar_constraint_test.go
+++ testing/optim/scalar_constraint_test.go
@@ -8,6 +8,7 @@ Description:
 
 import (
 	"github.com/MatProGo-dev/MatProInterface.go/optim"
+	"gonum.org/v1/gonum/mat"
 	"testing"
 )
 
@@ -29,6 +30,71 @@ func TestScalarConstraint_ScalarConstraint1(t *testing.T) {
 			"Scalar Constraint does not contain equality sense; received %v",
 			sc1.Sense,
 		)
+	}
+}
+
+/*
+TestScalarConstraint_IsLinear1
+Description:
+
+	Detects whether a simple inequality between
+	a variable and a constant is a linear constraint.
+*/
+func TestScalarConstraint_IsLinear1(t *testing.T) {
+	// Constants
+	m := optim.NewModel("scalar-constraint-test1")
+	v1 := m.AddVariable()
+	k1 := optim.K(2.8)
+
+	// Algorithm
+	sc1 := optim.ScalarConstraint{
+		LeftHandSide:  v1,
+		RightHandSide: k1,
+		Sense:         optim.SenseEqual,
+	}
+
+	tf, err := sc1.IsLinear()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if !tf {
+		t.Errorf("sc1 is linear, but function claims it is not!")
+	}
+
+}
+
+/*
+TestScalarConstraint_IsLinear1
+Description:
+
+	Detects whether a simple inequality between
+	a variable and a constant is a linear constraint.
+*/
+func TestScalarConstraint_IsLinear2(t *testing.T) {
+	// Constants
+	m := optim.NewModel("scalar-constraint-test1")
+	v1 := m.AddVariable()
+	sqe2 := optim.ScalarQuadraticExpression{
+		L: optim.OnesVector(1),
+		Q: *mat.NewDense(1, 1, []float64{3.14}),
+		X: optim.VarVector{Elements: []optim.Variable{v1}},
+	}
+
+	k1 := optim.K(2.8)
+
+	// Algorithm
+	sc1 := optim.ScalarConstraint{
+		LeftHandSide:  sqe2,
+		RightHandSide: k1,
+		Sense:         optim.SenseEqual,
+	}
+
+	tf, err := sc1.IsLinear()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if tf {
+		t.Errorf("sc1 is not linear, but function claims it is!")
 	}
 
 }
