7541505419262a98facb9062468777b7802bd177
Kwesi Rutledge <kwesir@mit.edu>
2024-12-16 09:27:45 -0400

    Added some tests for ScalarConstraint objects in the optim package

--- testing/optim/scalar_constraint_test.go
+++ testing/optim/scalar_constraint_test.go
@@ -7,9 +7,10 @@ Description:
 */
 
 import (
+	"testing"
+
 	"github.com/MatProGo-dev/MatProInterface.go/optim"
 	"gonum.org/v1/gonum/mat"
-	"testing"
 )
 
 func TestScalarConstraint_ScalarConstraint1(t *testing.T) {
@@ -64,7 +65,7 @@ func TestScalarConstraint_IsLinear1(t *testing.T) {
 }
 
 /*
-TestScalarConstraint_IsLinear1
+TestScalarConstraint_IsLinear2
 Description:
 
 	Detects whether a simple inequality between
@@ -86,6 +87,43 @@ func TestScalarConstraint_IsLinear2(t *testing.T) {
 	sc1 := optim.ScalarConstraint{
 		LeftHandSide:  sqe2,
 		RightHandSide: k1,
+		Sense:         optim.SenseEqual,
+	}
+
+	tf, err := sc1.IsLinear()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if tf {
+		t.Errorf("sc1 is not linear, but function claims it is!")
+	}
+
+}
+
+/*
+TestScalarConstraint_IsLinear3
+Description:
+
+	Detects whether a simple inequality between
+	a variable and a constant is a quadratic expression.
+	The function should identify that this is NOT a linear expression.
+*/
+func TestScalarConstraint_IsLinear3(t *testing.T) {
+	// Constants
+	m := optim.NewModel("scalar-constraint-test1")
+	v1 := m.AddVariable()
+	sqe2 := optim.ScalarQuadraticExpression{
+		L: optim.OnesVector(1),
+		Q: *mat.NewDense(1, 1, []float64{3.14}),
+		X: optim.VarVector{Elements: []optim.Variable{v1}},
+	}
+
+	k1 := optim.K(2.8)
+
+	// Algorithm
+	sc1 := optim.ScalarConstraint{
+		LeftHandSide:  k1,
+		RightHandSide: sqe2,
 		Sense:         optim.SenseEqual,
 	}
 
@@ -136,5 +174,99 @@ func TestScalarConstraint_Simplify1(t *testing.T) {
 
 	if float64(sc2.RightHandSide.(optim.K)) != 1.0 {
 		t.Errorf("Remainder on LHS was not contained properly")
+	}
+}
+
+/*
+TestScalarConstraint_Simplify2
+Description:
+
+	Attempts to simplify the constraint between
+	a scalar linear epression and a variable.
+	The resulting constraint should have a zero remainder on the right hand side
+	and the left hand side should contain a variable vector with 1 more element than it started with.
+*/
+func TestScalarConstraint_Simplify2(t *testing.T) {
+	// Constants
+	m := optim.NewModel("scalar-constraint-test2")
+	vv1 := m.AddVariableVector(3)
+	sle2 := optim.ScalarLinearExpr{
+		L: optim.OnesVector(vv1.Len()),
+		X: vv1,
+		C: 2.0,
+	}
+	v3 := m.AddVariable()
+
+	// Create sles
+	sc1 := optim.ScalarConstraint{
+		LeftHandSide:  sle2,
+		RightHandSide: v3,
+		Sense:         optim.SenseEqual,
+	}
+
+	// Attempt to simplify
+	sc2, err := sc1.Simplify()
+	if err != nil {
+		t.Errorf("unexpected error during simplify(): %v", err)
+	}
+
+	if float64(sc2.RightHandSide.(optim.K)) != 0.0 {
+		t.Errorf("Remainder on LHS was not contained properly")
+	}
+
+	if sc2.LeftHandSide.(optim.ScalarLinearExpr).X.Len() != 4 {
+		t.Errorf("Variable vector did not increase in size")
+	}
+}
+
+/*
+TestScalarConstraint_Simplify3
+Description:
+
+	Attempts to simplify the constraint between
+	a scalar linear epression and a scalar quadratic expression.
+*/
+func TestScalarConstraint_Simplify3(t *testing.T) {
+	// Constants
+	m := optim.NewModel("scalar-constraint-test3")
+	vv1 := m.AddVariableVector(3)
+	sle2 := optim.ScalarLinearExpr{
+		L: optim.OnesVector(vv1.Len()),
+		X: vv1,
+		C: 2.0,
+	}
+	sqe3 := optim.ScalarQuadraticExpression{
+		L: optim.OnesVector(3),
+		Q: *mat.NewDense(
+			3, 3,
+			[]float64{
+				3.14, 0.0, 0.0,
+				0.0, 3.14, 0.0,
+				0.0, 0.0, 3.14,
+			},
+		),
+		X: vv1,
+		C: 1.5,
+	}
+
+	// Create sles
+	sc1 := optim.ScalarConstraint{
+		LeftHandSide:  sle2,
+		RightHandSide: sqe3,
+		Sense:         optim.SenseLessThanEqual,
+	}
+
+	// Attempt to simplify
+	sc2, err := sc1.Simplify()
+	if err != nil {
+		t.Errorf("unexpected error during simplify(): %v", err)
+	}
+
+	if float64(sc2.RightHandSide.(optim.K)) != sqe3.C {
+		t.Errorf(
+			"Remainder on RHS was not contained properly; expected %v, received %v",
+			sqe3.C,
+			sc2.RightHandSide.(optim.K),
+		)
 	}
 }
